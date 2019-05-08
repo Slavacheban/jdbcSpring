c40d41d3060d40571b1cc231067bedf0daff721f
Slava <slavacheban22@gmail.com>
2019-05-08 21:01:33 +0300

    create tests

---
+++ src/test/java/service/PersonServiceImplTest.java
@@ -0,0 +1,28 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonServiceImplTest {
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void findPersonById() {
+    }
+
+    @Test
+    void updatePerson() {
+    }
+
+    @Test
+    void deletePersonById() {
+    }
+
+    @Test
+    void getAllPersons() {
+    }
+}
